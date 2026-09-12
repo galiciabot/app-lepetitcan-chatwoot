1afd078997ebfd584df6744088c5ba80d045bbb5
GaliciaBot <galiciabot@users.noreply.github.com>
2026-09-12 15:02:11 +0000

    chore: add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/app-lepetitcan-chatwoot/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
